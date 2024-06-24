db5675ab36090744f1a4da5b6b5e45e4f84ada46
Isaias Medina <isamed92@gmail.com>
2024-06-24 03:37:46 -0300

    chore: create AuthModule and signup endpoint

---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,34 @@
+import { IsArray, IsBoolean, IsString } from 'class-validator';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('text', { unique: true })
+  @IsString()
+  email: string;
+
+  @Column('text')
+  @IsString()
+  password: string;
+
+  @Column('text')
+  @IsString()
+  fullName: string;
+
+  @Column('bool', {
+    default: true,
+  })
+  @IsBoolean()
+  isActive: boolean;
+
+  @Column('text', {
+    array: true,
+    default: ['user'],
+  })
+  @IsString({ each: true })
+  @IsArray()
+  roles: string[];
+}
