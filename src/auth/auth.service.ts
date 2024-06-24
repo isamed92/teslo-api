4974c9ca1552a559321379688e61e258d4ff3dbc
Isaias Medina <isamed92@gmail.com>
2024-06-24 11:17:23 -0300

    feat: login implemented

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,12 +2,13 @@ import {
   BadRequestException,
   Injectable,
   InternalServerErrorException,
+  UnauthorizedException,
 } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import * as bcrypt from 'bcrypt';
+import { CreateUserDto, LoginUserDto } from './dto';
 
 @Injectable()
 export class AuthService {
@@ -32,6 +33,23 @@ export class AuthService {
     } catch (error) {
       this.handleDBErrors(error);
     }
+  }
+
+  async login(loginUserDto: LoginUserDto) {
+    const { password, email } = loginUserDto;
+
+    const user = await this.userRepository.findOne({
+      where: { email },
+      select: { email: true, password: true },
+    });
+
+    if (!user) throw new UnauthorizedException('Not valid credentials');
+
+    if (!bcrypt.compareSync(password, user.password))
+      throw new UnauthorizedException('Not valid credentials');
+
+    return user;
+    //TODO: retornar el JWT de acceso
   }
 
   private handleDBErrors(error: any): never {
--- src/auth/entities/user.entity.ts
+++ src/auth/entities/user.entity.ts
@@ -10,7 +10,9 @@ export class User {
   @IsString()
   email: string;
 
-  @Column('text')
+  @Column('text', {
+    // select: false,
+  })
   @IsString()
   password: string;
 
