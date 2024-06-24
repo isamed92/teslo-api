652ed0c41e136dd8c66cfd8b0f7ec2452d44a246
Isaias Medina <isamed92@gmail.com>
2024-06-23 21:00:44 -0300

    chore: Update npm dependencies and add @nestjs/serve-static package

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { ProductModule } from './product/product.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { FilesModule } from './files/files.module';
+// import { join } from 'path';
+// import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
   imports: [
@@ -19,6 +21,9 @@ import { FilesModule } from './files/files.module';
       autoLoadEntities: true,
       synchronize: true,
     }),
+    // ServeStaticModule.forRoot({
+    //   rootPath: join(__dirname, '..', 'public'),
+    // }),
     ProductModule,
     CommonModule,
     SeedModule,
