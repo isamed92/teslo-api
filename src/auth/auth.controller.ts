0958f6dd0c444837eade9d05246a4a6887d98f2d
Isaias Medina <isamed92@gmail.com>
2024-07-01 03:42:57 -0300

    feat: Add private route with authentication guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,18 @@
-import { Controller, Post, Body, Get, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  UseGuards,
+  SetMetadata,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginUserDto } from './dto';
 import { AuthGuard } from '@nestjs/passport';
+import { User } from './entities/user.entity';
+import { Auth, GetUser, RawHeaders, RoleProtected } from './decorators';
+import { UserRoleGuard } from './guards/user-role.guard';
+import { ValidRoles } from './interfaces';
 
 @Controller('auth')
 export class AuthController {
@@ -19,11 +30,41 @@ export class AuthController {
 
   @Get('private')
   @UseGuards(AuthGuard())
-  testingPrivateRoute() {
+  testingPrivateRoute(
+    // @Req() request: Express.Request,
+    @GetUser() user: User,
+
+    @GetUser('email') userEmail: string,
+
+    @RawHeaders() rawheaders: string[],
+  ) {
+    // const { user } = request;
     return {
       ok: true,
       message: 'hola mundo private',
-      user: { name: 'isaias' },
+      user,
+      userEmail,
+      rawheaders,
+    };
+  }
+
+  @Get('private2')
+  // @SetMetadata('roles', ['admin', 'superuser'])
+  @RoleProtected(ValidRoles.admin, ValidRoles.superUser)
+  @UseGuards(AuthGuard(), UserRoleGuard)
+  privateRoute2(@GetUser() user: User) {
+    return {
+      ok: true,
+      user,
+    };
+  }
+
+  @Get('private3')
+  @Auth(ValidRoles.superUser)
+  privateRoute3(@GetUser() user: User) {
+    return {
+      ok: true,
+      user,
     };
   }
 }
---
+++ src/auth/decorators/get-user.decorator.ts
@@ -0,0 +1,20 @@
+import {
+  createParamDecorator,
+  ExecutionContext,
+  InternalServerErrorException,
+} from '@nestjs/common';
+
+export const GetUser = createParamDecorator(
+  (data: string, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+
+    const user = request.user;
+
+    if (!user)
+      throw new InternalServerErrorException('User not found (request)');
+
+    if (data) return user[data];
+
+    return user;
+  },
+);
